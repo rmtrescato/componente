7bef3a8631ac67aee402edf380f36513b64cae3d
rmtrescato <rmenezestrescato@gmail.com>
2024-05-13 10:43:27 -0300

    criacao de Componente

---
+++ src/componentes/Conteudo.js
@@ -0,0 +1,10 @@
+function Conteudo(){
+ return (
+  <div>
+    <p><button> Conteudo </button></p>
+      <p> este componente funciona assim...</p>
+  </div>
+ )
+}
+
+export default Conteudo
---
+++ src/componentes/Meucomponente.js
@@ -0,0 +1,10 @@
+function Meucomponente(){
+ return (
+  <div>
+   testando meu componente
+  </div>
+ )
+}
+
+
+export default Meucomponente
